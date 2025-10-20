61c6a6f580f82ead34400d3d509db572be5d6ffd
Supporterino <lars@roth-kl.de>
2025-10-20 09:01:05 +0200

    feat: add CI workflow, upgrade PiHoleCluster CRD to v1, update RBAC and webhook definitions for new API version

--- internal/controller/piholecluster/piholecluster_controller.go
+++ internal/controller/piholecluster/piholecluster_controller.go
@@ -52,7 +52,6 @@ type Reconciler struct {
 
 // RBAC permissions -------------------------------------------------------
 
-//nolint:revive
 //+kubebuilder:rbac:groups=supporterino.de,resources=piholeclusters,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=supporterino.de,resources=piholeclusters/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=supporterino.de,resources=piholeclusters/finalizers,verbs=update
